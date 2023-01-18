b7ba826f9e0ddb61836aacd5346da3dcf6c0d4e9
Tony Ehlert <aehlert@dmacc.edu>
2023-01-18 11:10:53 -0600

    Initial commit and push from desktop repo

---
+++ List_Methods_Ehlert.py
@@ -0,0 +1,17 @@
+"""
+Program: List_Methods_Ehlert
+Author: Tony Ehlert
+Last date modified: 
+
+The purpose of this program is to get practice using list methods in Python
+The input is three strings that are put into a list
+The output is the results of the various list functions we are testing
+"""
+
+list_one = ["lift", "heavy", "often"]
+
+print(list_one)
+
+# use of clear() method
+list_one.clear()
+print(list_one)
---
+++ String_Methods_Ehlert.py
@@ -0,0 +1,17 @@
+"""
+Program: String_Methods_Ehlert.py
+Author: Tony Ehlert
+Last date modified: 1/18/2023
+
+The purpose of this program is to get practice using string methods in Python
+The input is a string variable
+The output is the string variable after using different string methods
+"""
+
+movie_string = "Tony Stark in Iron Man"
+
+# use of isspace() method
+print(movie_string.isspace())
+
+# use of startswith() method
+print(movie_string.startswith("Ton"))
